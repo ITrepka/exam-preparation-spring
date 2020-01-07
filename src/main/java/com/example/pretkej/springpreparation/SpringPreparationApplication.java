b2cb25e568277c30870081733a23a8c854e1c826
Ireneusz Trepka <irek.trepka@gmail.com>
2020-01-07 23:02:30 +0100

    init commit

---
+++ src/main/java/com/example/pretkej/springpreparation/SpringPreparationApplication.java
@@ -0,0 +1,13 @@
+package com.example.pretkej.springpreparation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPreparationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPreparationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pretkej/springpreparation/SpringPreparationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pretkej.springpreparation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPreparationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
